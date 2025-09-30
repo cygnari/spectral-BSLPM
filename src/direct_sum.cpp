1df5dfb2ae45e8f42f012bb9c411849db88dbfc4
Anthony Chen <cygnari@berkeley.edu>
2025-09-30 19:34:48 -0400

    Undo change to MD Range Policy

--- src/direct_sum.cpp
+++ src/direct_sum.cpp
@@ -29,7 +29,8 @@ void direct_sum_inv_lap(const RunConfig& run_config, Kokkos::View<double*> xcos,
 						Kokkos::View<double*> ycos, Kokkos::View<double*> zcos, 
 						Kokkos::View<double*> area, Kokkos::View<double*> pots, 
 						Kokkos::View<double*> soln) {
-	Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<2,Kokkos::Iterate::Left,Kokkos::Iterate::Left>>(Kokkos::DefaultExecutionSpace(), {run_config.oned_lb, 0},{run_config.oned_ub, run_config.point_count}), inv_lap(xcos, ycos, zcos, area, pots, soln));
+	// Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<2,Kokkos::Iterate::Left,Kokkos::Iterate::Left>>(Kokkos::DefaultExecutionSpace(), {run_config.oned_lb, 0},{run_config.oned_ub, run_config.point_count}), inv_lap(xcos, ycos, zcos, area, pots, soln));
+	Kokkos::parallel_for(Kokkos::MDRangePolicy(Kokkos::DefaultExecutionSpace(), {run_config.oned_lb, 0},{run_config.oned_ub, run_config.point_count}), inv_lap(xcos, ycos, zcos, area, pots, soln));
 	Kokkos::fence();
 
 	MPI_Allreduce(MPI_IN_PLACE, &soln(0), run_config.point_count, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
